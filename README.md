31657ff4e42966ab65dfadac1453eaee7fbe31a3
Wenty Li <wenty.l@nodereal.io>
2023-03-02 12:09:24 +0800

    Update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # yaml2env
 yaml to env variables
+
+
+ncc build index.js --license licenses.txt 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 const yaml = require('js-yaml');
 const fs   = require('fs');
 
